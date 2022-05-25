57d528777907eaefea45c0188a9bb191bee5a2d1
William <williamsantana5750@gmail.com>
2022-05-25 11:17:22 -0300

    Delete Function - Import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
-import { uploadRouter } from "./importTodos.routes";
 import { todosRoutes } from "./todos.routes";
 
 const router = Router();
 
 router.use("/todos", todosRoutes);
-router.use("/upload", uploadRouter);
 
 export { router };
--- src/useCase/uploadArquivo/index.ts
+++
@@ -1,9 +0,0 @@
-import { TodosRepository } from "../../repositories/implementation/TodosRepository";
-import { ImportTodoController } from "./ImportTodoController";
-import { ImportTodoUseCase } from "./ImportTodoUseCase";
-
-const todoRepository = TodosRepository.getInstance();
-const importTodoUseCase = new ImportTodoUseCase(todoRepository);
-const importTodoController = new ImportTodoController(importTodoUseCase);
-
-export { importTodoController };
