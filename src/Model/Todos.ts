883750e45997e8e3c30ac4ec66e07065c67e5760
William <williamsantana5750@gmail.com>
2022-05-26 21:01:17 -0300

    Migration : ormConfig, migration, Model

--- src/Model/Todos.ts
+++ src/Model/Todos.ts
@@ -1,10 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity()
 class Todos {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   author: string;
+  @Column()
   description: string;
+  @CreateDateColumn()
   created_at: Date;
+  @UpdateDateColumn()
   update_at?: Date;
 
   constructor() {
---
+++ src/database/migrations/1653609001359-CreateTodos.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTodos1653609001359 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "todos",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+          },
+          {
+            name: "author",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("todos");
+  }
+}
